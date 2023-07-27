aa4b6ec73ec28fcbfb8a54f2f508915a9d9fe03f
splatter <splatterxl@outlook.ie>
2023-07-27 12:39:52 +0100

    start work on botum

--- src/bot.ts
+++ src/bot.ts
@@ -1,13 +1,18 @@
 import {
 	APIInteraction,
+	APIInteractionResponse,
 	APIMessageComponentInteraction,
+	ComponentType,
 	InteractionResponseType,
-	InteractionType, RouteBases, Routes
+	InteractionType,
+	MessageFlags,
+	RouteBases,
+	Routes,
+	TextInputStyle
 } from 'discord-api-types/v10';
-import type { Env } from './worker';
-import { Email } from 'postal-mime';
+import type {Env} from './worker';
 
-export const handle = async (event: APIInteraction, env: Env) => {
+export const handle = async (event: APIInteraction, env: Env): Promise<APIInteractionResponse> => {
 	switch (event.type) {
 		case InteractionType.Ping:
 			return {
@@ -20,7 +25,8 @@ export const handle = async (event: APIInteraction, env: Env) => {
 				case 'delete': {
 					await request(Routes.channelMessage(interaction.channel_id, interaction.message.id), {
 						method: 'DELETE',
-					}, env)
+					}, env);
+
 					return {
 						type: InteractionResponseType.DeferredMessageUpdate
 					}
@@ -31,42 +37,68 @@ export const handle = async (event: APIInteraction, env: Env) => {
 					if (!embed) return {
 						type: InteractionResponseType.ChannelMessageWithSource,
 						data: {
-							content: 'No data embed found'
+							content: 'No data embed found',
+							flags: MessageFlags.Ephemeral
 						}
 					}
 
-					const data: Email = JSON.parse(atob(embed));
+					const data: {subject: string} = JSON.parse(atob(embed));
 
-					await fetch('https://api.mailchannels.net/tx/v1/send', {
-						method: 'POST',
-						headers: {
-							'content-type': 'application/json',
-						},
-						body: JSON.stringify({
-							personalizations: [
+					return {
+						type: InteractionResponseType.Modal,
+						data: {
+							title: `Re: ${data.subject.replace(/^(Re: )+/g, '').slice(0, 1024)}`,
+							custom_id: 'reply',
+							components: [
 								{
-									to: [{ email: 'test@example.com', name: 'Test Recipient' }],
+									type: ComponentType.ActionRow,
+									components: [
+										{
+											type: ComponentType.TextInput,
+											custom_id: 'subject',
+											required: true,
+											placeholder: 'Subject line in email',
+											label: "Subject",
+											value: `Re: ${data.subject.replace(/^(Re: )+/g, '')}`,
+											style: TextInputStyle.Short,
+											min_length: 1,
+											max_length: 1024
+										}
+									]
 								},
-							],
-							from: {
-								email: 'sender@example.com',
-								name: 'Workers - MailChannels integration',
-							},
-							subject: 'Look! No servers',
-							content: [
 								{
-									type: 'text/plain',
-									value: 'And no email service accounts and all for free too!',
-								},
-							],
-						}),
-					})
+									type: ComponentType.ActionRow,
+									components: [
+										{
+											type: ComponentType.TextInput,
+											custom_id: 'body',
+											required: true,
+											label: "Body",
+											style: TextInputStyle.Paragraph,
+											min_length: 1,
+											max_length: 2000
+										}
+									]
+								}
+							]
+						}
+					}
+				}
+				default: {
+					return {
+						type: InteractionResponseType.ChannelMessageWithSource,
+						data: {
+							content: 'Unsupported custom id',
+							flags: MessageFlags.Ephemeral
+						}
+					}
 				}
 			}
 		}
 		default:
 			return {
-
+				type: InteractionResponseType.ChannelMessageWithSource,
+				data: { content:"Unsupported interaction type", flags: MessageFlags.Ephemeral }
 			}
 	}
 }
--- src/worker.ts
+++ src/worker.ts
@@ -1,13 +1,15 @@
+// noinspection JSUnusedGlobalSymbols
+
 import {
 	ButtonStyle,
 	ComponentType,
 	RESTPostAPIWebhookWithTokenJSONBody,
 	RouteBases,
-	Routes
-} from 'discord-api-types/v10';
-import PostalMime from 'postal-mime';
-import { verify } from './verify';
-import { handle } from './bot';
+	Routes,
+} from "discord-api-types/v10";
+import PostalMime from "postal-mime";
+import { verify } from "./verify";
+import { handle } from "./bot";
 
 async function streamToArrayBuffer(stream: ReadableStream, streamSize: number) {
 	let result = new Uint8Array(streamSize);
@@ -29,6 +31,7 @@ export interface Env {
 	CHANNEL_ID: string;
 	DISCORD_TOKEN: string;
 	PUBLIC_KEY: string;
+	GITHUB_TOKEN: string;
 }
 
 export default {
@@ -37,24 +40,97 @@ export default {
 		const parser = new PostalMime();
 		const email = await parser.parse(rawEmail);
 
-		email.attachments = email.attachments.filter(v => v.filename !== 'signature.asc');
+		email.attachments = email.attachments.filter(
+			(v) => v.filename !== "signature.asc",
+		);
 
-		if (email.attachments.length > 10) event.forward(env.FALLBACK_EMAIL);
+		try {
+			// still forward in case the request fails: body too long, too many attachments, etc.
+			await event.forward(env.FALLBACK_EMAIL);
+		} catch {
+			console.error("Could not forward to original fallback email address.");
+		}
 
 		const formData = new FormData();
 
 		const caseId = crypto.randomUUID();
 
-		const data = btoa(JSON.stringify({
-			replyTo: email.replyTo ?? [email.from],
-			caseId,
-			body: email.text
-		}));
+		const data = btoa(
+			JSON.stringify({
+				replyTo: email.replyTo ?? [email.from],
+				caseId,
+				subject: email.subject || "(no subject)",
+				inReplyTo: email.inReplyTo,
+				messageId: email.messageId,
+			}),
+		);
+
+		let gist: string | null = null;
+		let html: string | null = null;
+
+		if (env.GITHUB_TOKEN) {
+			try {
+				const res = await fetch(`https://api.github.com/gists`, {
+					method: "POST",
+					body: JSON.stringify({
+						description:
+							"📩️ New message to **${event.to}**!%${email.attachments.length > 10 ? ' Extra attachments were forwarded to the fallback email address.' : ''}",
+						public: false,
+						files: {
+							"headers.txt": {
+								content: `
+							From: ${email.from.name} <${email.from.address}>
+							To: ${email.to.map((v) => v.name + " <" + v.address + ">").join(", ")}
+							Cc: ${email.cc?.map((v) => v.name + " <" + v.address + ">").join(", ") ?? ""}
+							Bcc: ${email.bcc?.map((v) => v.name + " <" + v.address + ">").join(", ") ?? ""}
+							Subject: ${email.subject}
+							
+							Reply-To: ${email.replyTo
+									.map((v) => v.name + " <" + v.address + ">")
+									.join(", ")}
+							${email.headers.map((h) => `${h.name}: ${h.value}`).join("\n")}
+						`,
+							},
+							"readme.html": { content: email.html },
+							"readme.txt": { content: email.text },
+							...Object.fromEntries(
+								email.attachments.map((a) => [
+									`attachment-${a.filename}`,
+									{ content: a.content },
+								]),
+							),
+						},
+					}),
+					headers: {
+						Authorization: `Bearer ${env.GITHUB_TOKEN}`,
+						"X-GitHub-Api-Version": "2022-11-28",
+					},
+				});
+
+				if (!res.ok) {
+					console.error(`Could not upload gist ${res.status}: ${await res.text()}`);
+				} else {
+					const json = <any>await res.json();
+					gist = json.html_url;
+					html =
+						"https://htmlpreview.github.io/?" +
+						json.files["readme.txt"].raw_url;
+
+					console.log(`gist ${gist} html ${html}`)
+				}
+			} catch (e) {
+				console.error("Could not upload to gist: " + e.toString());
+			}
+		}
 
 		formData.append(
 			"payload_json",
 			JSON.stringify({
-				content: `📩️ New message to **${event.to}**!`,
+				content: `📩️ New message to **${event.to}**!%${
+					email.attachments.length > 10
+						? " Extra attachments were forwarded to the fallback email address."
+						: ""
+				}`,
 				embeds: [
 					{
 						title: email.subject,
@@ -103,17 +179,21 @@ export default {
 									name: header.name.replace(/-/g, " "),
 									value: header.value,
 								})),
+							{
+								name: "Message ID",
+								value: email.messageId,
+							},
+							{
+								name: "Replying to",
+								value: email.inReplyTo ?? "New thread",
+							},
 						],
 						timestamp: email.date && new Date(email.date).toISOString(),
 						footer: {
-							text: `${email.attachments.length} attachment(s) • ${
-								rawEmail.length
-							} bytes ${
-								email.inReplyTo ? "• reply to " + email.inReplyTo : ""
-							}`,
+							text: `${email.attachments.length} attachment(s) • ${rawEmail.length} bytes`,
 						},
 					},
-					{description: data }
+					{ description: data },
 				],
 				components: [
 					{
@@ -125,20 +205,30 @@ export default {
 								label: "Reply",
 								style: ButtonStyle.Primary,
 							},
-							{
-								type: ComponentType.Button,
-								custom_id: "forward",
-								label: "Forward",
-								style: ButtonStyle.Secondary,
-							},
+							...((gist &&
+									html && [
+										{
+											type: ComponentType.Button,
+											label: "View full",
+											style: ButtonStyle.Link,
+											url: gist,
+										},
+										{
+											type: ComponentType.Button,
+											label: "View HTML",
+											style: ButtonStyle.Link,
+											url: html,
+										},
+									]) ||
+								[]),
 							{
 								type: ComponentType.Button,
 								custom_id: "delete",
 								label: "Delete",
 								style: ButtonStyle.Danger,
-							}
+							},
 						],
-					}
+					},
 				],
 				attachments: email.attachments
 					.slice(0, 10)
@@ -149,35 +239,55 @@ export default {
 			} as RESTPostAPIWebhookWithTokenJSONBody),
 		);
 
-		for (const [index, attachment] of email.attachments.slice(0, 10).entries()) {
+		for (const [index, attachment] of email.attachments
+			.slice(0, 10)
+			.entries()) {
 			formData.append(
 				`files[${index}]`,
-				new Blob(
-					[attachment.content],
-					{ type: attachment.mimeType }
-				),
-				attachment.filename
+				new Blob([attachment.content], { type: attachment.mimeType }),
+				attachment.filename,
 			);
 		}
-		const result = await fetch(`${RouteBases.api}${Routes.channel(env.CHANNEL_ID)}/messages`, {
-			method: "POST",
-			body: formData,
-			headers: {
-				"User-Agent": "DiscordBot (https://github.com/splatterxl/email-in-discord; v1.0.0)",
-				Authorization: `Bot ${env.DISCORD_TOKEN}`
+		const result = await fetch(
+			`${RouteBases.api}${Routes.channel(env.CHANNEL_ID)}/messages`,
+			{
+				method: "POST",
+				body: formData,
+				headers: {
+					"User-Agent":
+						"DiscordBot (https://github.com/splatterxl/email-in-discord; v1.0.0)",
+					Authorization: `Bot ${env.DISCORD_TOKEN}`,
+				},
 			},
-		});
+		);
 
 		if (!result.ok) {
 			console.error(await result.text());
+
+			event.setReject("Invalid upstream request. " + result.status);
 
 			throw new Error("Invalid request: " + result.status);
 		}
 	},
 	async fetch(request: Request, env: Env, ctx: ExecutionContext) {
-		if (!request.headers.get('X-Signature-Ed25519') || !request.headers.get('X-Signature-Timestamp')) return Response.redirect('https://github.com/splatterxl/email-in-discord')
-		if (!await verify(request, env.PUBLIC_KEY)) return new Response('Unauthorized', { status: 401 })
+		if (
+			!request.headers.get("X-Signature-Ed25519") ||
+			!request.headers.get("X-Signature-Timestamp")
+		)
+			return Response.redirect(
+				"https://github.com/splatterxl/email-in-discord",
+			);
+		if (!(await verify(request, env.PUBLIC_KEY)))
+			return new Response("Unauthorized", { status: 401 });
 
-		return new Response(JSON.stringify(handle(await request.json(), env)))
-	}
+		return new Response(
+			JSON.stringify(await handle(await request.json(), env)),
+			{
+				status: 200,
+				headers: {
+					"Content-Type": "application/json",
+				},
+			},
+		);
+	},
 };
